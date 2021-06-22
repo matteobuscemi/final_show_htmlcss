21c2b1648126f6db67d1121f4676a2241cf3a3c7
matteobuscemi <43605898+matteobuscemi@users.noreply.github.com>
2021-06-22 12:10:26 +0200

    script.js & showroommenu.js => klaar inladen van alle projecten + menu

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
 const api_url = "https://finalshowcase.herokuapp.com/final-work/get-all";
 
-var buttons = document.querySelectorAll("a");
-var cluster = "web";
+var buttons = document.querySelectorAll(".bottom-menu a");
+var cluster = localStorage.getItem("cluster")||"web";
+document.querySelector(`a.${cluster}`).classList.add("active");
 buttons.forEach(button => {
     button.addEventListener("click", function(){
         cluster = button.classList[0];
-        console.log(cluster);
+        button.classList.add("active");
+        var children = [].slice.call(button.parentNode.children);
+        children.forEach(child => {
+            if(child.classList[0] != cluster) {
+                child.classList.remove("active");
+            }
+        });
         getapi(api_url);
     })
 });
-
-
 
 
 
@@ -26,24 +31,23 @@ async function getapi(url) {
 getapi(api_url);
 
 function show(data) {
-    let projecten = `
-    <img class="coverphoto" src="../../img/dummyphotosr.png">
-            
-            <h2>Title Title</h2>
-            <h3>Firstname Lastname</h3>
-            <h4>Beschrijving</h4>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat<span id="dots">...</span>
-                <span id="meer">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                    <button onclick="myFunction()" id="myBtn">Lees meer</button>
-    `
-    var project = data.find(p => p.cluster == cluster);
-    var tekst2 = project.description.split(".");
-    var tekst1 = tekst2.shift();
-    console.log(project);
-
-    
+    var started = false;
+    var projecten = data.filter(p => p.cluster == cluster);
+    Array.prototype.next = function() {
+        return this[++this.current];
+    };
+    Array.prototype.prev = function() {
+        return this[--this.current];
+    };
+    Array.prototype.current = 0;
+    document.querySelector(".hoeveelheid").innerHTML = `${projecten.current+1}/${projecten.length}`;
+    var project = projecten[0];
+    var nextProject = document.querySelector(".right");
+    var previousProject = document.querySelector(".left");
+    nextProject.addEventListener("click", function(){
+        project = projecten.next();
+        document.querySelector(".hoeveelheid").innerHTML = `${projecten.current+1}/${projecten.length}`;
+        console.log("text");
         document.querySelector(".projecten").innerHTML = `
         <img class="coverphoto" src="${project.images}">
         <h2>${project.name}</h2>
@@ -51,5 +55,31 @@ function show(data) {
         <h4>Beschrijving</h4>
         <p>${project.description}</p>
         `;
+    });
+    previousProject.addEventListener("click", function(){
+        project = projecten.prev();
+        document.querySelector(".hoeveelheid").innerHTML = `${projecten.current+1}/${projecten.length}`;
+        console.log("text");
+        document.querySelector(".projecten").innerHTML = `
+        <img class="coverphoto" src="${project.images}">
+        <h2>${project.name}</h2>
+        <h3>${project.username}</h3>
+        <h4>Beschrijving</h4>
+        <p>${project.description}</p>
+        `;
+        
+    });
+
+
+    if(!started) {
+        document.querySelector(".projecten").innerHTML = `
+        <img class="coverphoto" src="${project.images}">
+        <h2>${project.name}</h2>
+        <h3>${project.username}</h3>
+        <h4>Beschrijving</h4>
+        <p>${project.description}</p>
+        `;
+        started = true;
+    }
 }
   
