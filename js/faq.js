fe120175878b67120dadc0edcbfd839430e539f7
yousra boutaarourte <yousra.boutaarourte@student.ehb.be>
2021-06-23 12:02:58 +0200

    feature_faqpage_changes

--- js/faq.js
+++ js/faq.js
@@ -1,31 +1,29 @@
 "use strict";
-document.getElementById("vraag1").addEventListener('click', function(e){
-    e.preventDefault;
-    console.log('succes!');
-    document.getElementById("titel").innerHTML = 'Wat is de Final Show?';
-    document.getElementById("info").innerHTML = 'De Final Show is een evenement waar eindejaarsstudenten van de opleiding Multimedia & Communicatietechnologie aan de Erasmushogeschool Brussel in de spotlight worden gezet. Hier kunnen ze tonen waar ze de voorbije maanden mee bezig zijn geweest. Dit is ook de ideale gelegenheid om contacten te leggen tussen werkgevers en potentiële werknemers';
-});
-document.getElementById("vraag2").addEventListener('click', function(e){
-    e.preventDefault;
-    console.log('succes!');
-    document.getElementById("titel").innerHTML = 'Hoe zal de Final Show doorgaan?';
-    document.getElementById("info").innerHTML = 'Dit jaar zal de Final Show te zien zijn via een livestream. De livestream kan je bekijken zonder een account aan te maken. Enkel wanneer je wilt chatten zullen we je vragen om snel een account aan te maken.';
-});
-document.getElementById("vraag3").addEventListener('click', function(e){
-    e.preventDefault;
-    console.log('succes!');
-    document.getElementById("titel").innerHTML = 'Waarom hapert de livestream?';
-    document.getElementById("info").innerHTML = 'Een stabiele internetconnectie is nodig om de livestream vloeiend te kunnen bekijken. Sluit ook onnodige openstaande tabs af, dit kan uw toestel en dus de stream vertragen.';
-});
-document.getElementById("vraag4").addEventListener('click', function(e){
-    e.preventDefault;
-    console.log('succes!');
-    document.getElementById("titel").innerHTML = 'Waar vind ik meer info over de opleiding?';
-    document.getElementById("info").innerHTML = 'Meer info hierover kan je op de website van EHB vinden.';
-});
-document.getElementById("vraag5").addEventListener('click', function(e){
-    e.preventDefault;
-    //console.log('succes!');
-    document.getElementById("titel").innerHTML = 'Hoe kan ik de school contacteren?';
-    document.getElementById("info").innerHTML = 'Meer info hierover kan je op de website van EHB vinden.';
+var faqButtons = document.querySelectorAll(".item");
+console.log(faqButtons);
+faqButtons.forEach(button => {
+   
+    button.addEventListener("click",function(){
+        var showEvent = button.classList[1];
+        console.log(showEvent);
+        let childs = [].slice.call(button.parentNode.children);
+        childs.forEach(child => {
+            console.log(child.classList[1]);
+            if(child.classList[1] != showEvent) {
+                child.classList.remove("active");
+                button.classList.add("active");
+            }
+        });
+           let siblings = document.querySelectorAll(".results");
+        siblings.forEach(sibling => {
+            if(sibling.classList[1] != showEvent) {
+
+                sibling.style.display ='none';
+            }
+            else {
+                sibling.style.display ='block';
+            }
+        });
+        
+    })
 });
