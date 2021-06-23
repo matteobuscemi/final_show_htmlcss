0fd4a3b448192e10c52e89b175543ebbedb034a6
matteobuscemi <43605898+matteobuscemi@users.noreply.github.com>
2021-06-23 19:47:48 +0200

    style.css => merge van files en style.css aangepast

--- js/faq.js
+++ js/faq.js
@@ -1,14 +1,11 @@
 "use strict";
 var faqButtons = document.querySelectorAll(".items");
-//console.log(faqButtons);
 faqButtons.forEach(button => {
    
     button.addEventListener("click",function(){
         var showEvent = button.classList[1];
-       //console.log(showEvent);
         let childs = [].slice.call(button.parentNode.children);
         childs.forEach(child => {
-           // console.log(child.classList[1]);
             if(child.classList[1] != showEvent) {
                 child.classList.remove("active");
                 button.classList.add("active");
